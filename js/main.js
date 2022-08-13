9efb8c11c6c1138009a849eeca1ed3ef68e671cb
Gyozo Miklos <nyosdroid@gmail.com>
2022-08-13 02:03:10 +0200

    CPE1704TKS Joshua is complete

--- js/main.js
+++ js/main.js
@@ -23,7 +23,9 @@ Joshua: A strange game. The only winning move is not to play. How about a nice g
 
 -Wargames (1983), http://www.imdb.com/title/tt0086567/trivia?tab=qt&ref_=tt_trv_qu
 
-https://www.youtube.com/watch?v=A45p48rDlwQ
+see also: https://www.youtube.com/watch?v=A45p48rDlwQ
+
+Nyos
 */
 
 const URLs = 
@@ -51,7 +53,7 @@ const getCleanState = () => // create a new state (this would be a constructor i
         next: Math.random()<.5?X:O, // next player to move, a random player starts the game
         lastMove: null, // holds the last valid move's index
         numberOfMoves: 0,
-        board: new Array(BOARD_SIZE).fill(_) // board state as a flat Array of 9 integers.. no need to complicate deep copy even further
+        board: new Array(BOARD_SIZE).fill(_) // board state as a 1D Array of 9 integers.. no need to complicate deep copy even further
     }
 }
 
@@ -91,7 +93,8 @@ const setDrawVisible = status => drawDiv.style.display = status ? 'inline-block'
 const setMessage = (msg) => messageSpan.innerHTML=msg
 
 // deep copy a state
-const getStateCopy = state => { // it seems JS has no deep copy
+const getStateCopy = state => // it seems JS has no deep copy
+{
     let newState = {...state}
     newState.board = Array.from(state.board)
     return newState
@@ -116,31 +119,11 @@ const step = (state, index) =>
     state.numberOfMoves += 1
 }
 
-// event handler
-const divPressed = index =>
-{
-    if(!active || !validMove(state, index))
-        return
-    
-    step(state, index)
-    printState(state)
-    const anyoneWon = whoWon(state)
-    if(anyoneWon!==null)
-    {
-        active = false
-        setMessage('WINNER: ' + winnerName[anyoneWon])
-        if(anyoneWon === 0)
-            setDrawVisible(true)
-    }
-}
-
-// add the event handler to the squares
-squares.forEach((sq, idx) => sq.addEventListener('click', ()=>divPressed(idx)))
-
 // the only valid move is to put a mark on an _empty_ square
 const validMove = (state, index) => state.board[index] === 0
 
 // try to determine who won (if any)
+// this has to be fast, there are a lot of boards to evaluate
 const whoWon = state =>
 {
     const possible = -state.next // possible winner can only be the last player who put a mark on the board
@@ -173,17 +156,107 @@ const whoWon = state =>
 
 const getNumberOfPlayers = () => Number.parseInt(document.getElementsByName('number_of_players')[0].value)
 
+const WINNING_VALUE = 1000
+const LOSING_VALUE = -1000
+const DRAW_VALUE = 0
+
+// technically, a simplified variant of https://en.wikipedia.org/wiki/Negamax
+// game state is small enough to be searched entirely, no need for maximum depth
+const miniMax = (state) =>
+{
+    const anyoneWon = whoWon(state) // if the game is finished, the evaluation is exact
+    if(anyoneWon!==null)
+    {
+        if(anyoneWon === 0)
+            return [DRAW_VALUE, null]
+        if(state.next === anyoneWon)
+            return [WINNING_VALUE, null]
+        return [LOSING_VALUE, null] // this should be unnecessary
+    }
+
+    // "opening book", just to be less boring
+    if(state.numberOfMoves === 0)
+    {
+        if(Math.random()<0.4) // put in the center most of the time
+            return [0, 4]
+        return [0, Math.floor(Math.random() * 5) * 2] // or corner/center
+    }
+
+    let bestValue = LOSING_VALUE
+    let bestMove
+
+    for(let i = 0; i < BOARD_SIZE; i++)
+    {
+        if(!validMove(state, i))
+            continue
+        let newState = getStateCopy(state)
+        step(newState, i) // try this move
+        let result = miniMax(newState) // evaluate this new state
+        let value = -result[0]
+        if(value === WINNING_VALUE)
+            return [value, i] // if Joshua can win, do that.. even if there are even better moves
+        if((value > bestValue) || ((value >= bestValue) && (Math.random()<0.2)))
+        {
+            bestValue = value
+            bestMove = i
+        }
+    }
+
+    return [bestValue, bestMove]
+}
+
 // start button
 const startGame = () =>
 {
     numberOfPlayers = getNumberOfPlayers()
-    if(numberOfPlayers === 0)
+    if(numberOfPlayers === 0) // play movie clip :)
         redirect()
     active = true
     state = getCleanState()
     printState(state)
     setDrawVisible(false)
+    Joshua(state)
 }
+
+const stepAndCheck = (state, index) =>
+{
+    step(state, index)
+    printState(state)
+    const anyoneWon = whoWon(state)
+    if(anyoneWon!==null)
+    {
+        active = false
+        setMessage('WINNER: ' + winnerName[anyoneWon])
+        if(anyoneWon === 0)
+            setTimeout(()=>setDrawVisible(true), 500)
+    }
+}
+
+// handles Joshua's turn.. called after the player's move or game start
+const Joshua = (state) =>
+{
+    if(numberOfPlayers === 1 && state.next === O)
+    {
+        let result = miniMax(state)
+        stepAndCheck(state, result[1])
+    }
+}
+
+// event handler
+const divPressed = index =>
+{
+    if(!active || !validMove(state, index))
+        return // not in active game or not a valid move
+    
+    if(numberOfPlayers === 1 && state.next == O)
+        return // Joshua's turn
+    
+    stepAndCheck(state, index)
+    Joshua(state)
+}
+
+// add the event handler to the squares
+squares.forEach((sq, idx) => sq.addEventListener('click', ()=>divPressed(idx)))
 
 
 
